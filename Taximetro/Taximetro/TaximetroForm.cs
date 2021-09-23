8494126a37c301b096c3b07ba631643d0c50d50d
Murilo Siqueira <msv2079@gmail.com>
2021-09-23 13:18:54 -0300

    Ajustes para colocar horário de inicio e esconder textos com  horarios

--- Taximetro/Taximetro/TaximetroForm.cs
+++ Taximetro/Taximetro/TaximetroForm.cs
@@ -8,7 +8,9 @@ namespace Taximetro
 	public partial class TaximetroForm : Form
 	{
 		private double _valorHora = 0;
-		private DateTime _dataHoraInicio;
+		private DateTime _hoje;
+		private bool _exibirHorario = false;
+		private DateTime _horarioIniciado = DateTime.MinValue;
 
 		public TaximetroForm()
 		{
@@ -25,22 +27,47 @@ namespace Taximetro
 			{
 				_valorHora = 1;
 			}
+
+			_exibirHorario = "1".Equals(ConfigurationManager.AppSettings["ExibirHorario"]);
+
+			var horarioIniciado = ConfigurationManager.AppSettings["HorarioIniciado"];
+
+			if (horarioIniciado.Contains(":"))
+			{
+				var splitHorario = horarioIniciado.Split(':');
+				var strHora = splitHorario[0];
+				var strMinuro = splitHorario[1];
+
+				_hoje = DateTime.Now;
+
+				if (int.TryParse(strHora, out var hora) && int.TryParse(strMinuro, out var minuto))
+				{
+					_horarioIniciado = new DateTime(_hoje.Year, _hoje.Month, _hoje.Day, hora, minuto, 0);
+				}
+			}
 		}
 
 		private void LigarTaximetroButton_Click(object sender, EventArgs e)
 		{
 			TaximetroTimer.Start();
 
-			_dataHoraInicio = DateTime.Now;
+			_hoje = DateTime.Now;
+
+			if(_horarioIniciado != DateTime.MinValue)
+			{
+				_hoje = _horarioIniciado;
+			}
 
 			HoraInicioLabel.Text = $"Hora Início: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}";
+			HoraInicioLabel.Visible = _exibirHorario;
+			TempoTrabalhoLlabel.Visible = _exibirHorario;
 		}
 
 		private void TaximetroTimer_Tick(object sender, EventArgs e)
 		{
 			DateTime agora = DateTime.Now;
 
-			var difH = agora.Subtract(_dataHoraInicio);
+			var difH = agora.Subtract(_hoje);
 
 			TempoTrabalhoLlabel.Text = $"Tempo Trabalho:{new DateTime(difH.Ticks).ToString("HH:mm:ss")}";
 
